3ba05e072831ec5a8491880aad8c00538ce18de9
onkvyawhare <onkvyawhare@gmail.com>
2024-05-02 22:38:13 +0530

    final commit2

--- class/src/App.js
+++ class/src/App.js
@@ -1,35 +1,33 @@
+
+import "./App.css";
+
 import React, { Component } from "react";
 
-import './App.css';
-
-export default class App extends Component{
-  constructor(){
+export default class App extends Component {
+  constructor() {
     super();
-    this.state={
-      count:0,
+    this.state = {
+      count: 0,
     };
-
-    this.increment=this.increment.bind(this);
-    this.decrement=this.decrement.bind(this);
-
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
 
-  increment(){
-    this.setState((state)=>({count:state.count+1}));
+  increment() {
+    this.setState((state) => ({ count: state.count + 1 }));
   }
 
-  decrement(){
-    this.setState((state)=>({count:state.count-1}));
+  decrement() {
+    this.setState((state) => ({ count: state.count - 1 }));
   }
-
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <h1>Counter App</h1>
-        <p>Count:{this.state.count}</p>
+        <p>Count: {this.state.count}</p>
         <button onClick={this.increment}>Increment</button>
         <button onClick={this.decrement}>Decrement</button>
       </div>
-    )
+    );
   }
 }
