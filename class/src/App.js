f885b908a3f7958c0cadbc0d4ae0cb842111d8f4
onkvyawhare <onkvyawhare@gmail.com>
2024-05-02 22:32:30 +0530

    final commit

--- class/src/App.js
+++ class/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      count:0,
+    };
 
-export default App;
+    this.increment=this.increment.bind(this);
+    this.decrement=this.decrement.bind(this);
+
+  }
+
+  increment(){
+    this.setState((state)=>({count:state.count+1}));
+  }
+
+  decrement(){
+    this.setState((state)=>({count:state.count-1}));
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Counter App</h1>
+        <p>Count:{this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    )
+  }
+}
